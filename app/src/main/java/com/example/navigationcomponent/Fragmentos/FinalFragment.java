d1fb46dfa114f3d1dc333af8237bf33b46ebc267
Manuel <mjvillar@iesoretania.es>
2022-10-20 08:42:43 +0200

    Añadidas las action de los demás destinos.

--- app/src/main/java/com/example/navigationcomponent/Fragmentos/FinalFragment.java
+++ app/src/main/java/com/example/navigationcomponent/Fragmentos/FinalFragment.java
@@ -5,22 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.navigationcomponent.R;
 
+public class FinalFragment extends Fragment {
 
-public class SegundoFragment extends Fragment {
-
-
-    public SegundoFragment() {
+    public FinalFragment() {
         // Required empty public constructor
     }
 
@@ -28,28 +24,20 @@ public class SegundoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segundo, container, false);
+        return inflater.inflate(R.layout.fragment_final, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final NavController navController = Navigation.findNavController(view);
-
-        TextView text1 = view.findViewById(R.id.textViewTexto);
-        TextView text2 = view.findViewById(R.id.textViewNumero);
-        Button boton =view.findViewById(R.id.idBotonFinal);
-
-        text1.setText(getArguments().getString("texto"));
-        text2.setText(String.valueOf(getArguments().getInt(("numero"))));
+        Button boton =view.findViewById(R.id.idBotonUltimo);
 
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                navController.navigate(R.id.action_segundoFragment_to_finalFragment);
+                Navigation.findNavController(view).navigate(R.id.action_finalFragment_to_inicioFragment);
             }
         });
-
     }
 }
